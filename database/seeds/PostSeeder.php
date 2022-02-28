a60ea5048562335f5c273ad3afdffa928b8a7f7d
ChriEffe <christianferrazzano93@gmail.com>
2022-02-28 15:26:53 +0100

    db seed post

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use App\Model\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,14 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(3, true);
+            $newPost->content = $faker->paragraphs(5, true);
+            $newPost->slug = Str::slug($newPost->title . '-' . $i, '-');
+            $newPost->save();
+        }
     }
 }
