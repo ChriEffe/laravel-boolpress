ae1b1f836090dc934780eeba35e0473932a0a309
ChriEffe <christianferrazzano93@gmail.com>
2022-02-28 15:58:44 +0100

    add create, index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('documentTitle')
+    {{-- {{ $title }} --}}
+@endsection
+
+@section('content')
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="container">
+        <div class="row">
+            <h1 class="h1">Admin - All posts</h1>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">Add new post</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <table class="table table-primary">
+                    <thead>
+                        <tr class="table-primary">
+                            <th>Title</th>
+                            <th>Author</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->content }}</td>
+                            <td>
+                                <a class="btn btn-primary" href="{{ route('admin.posts.show', $post) }}">View</a>
+                                <a class="btn btn-primary" href="{{ route('admin.posts.edit', $post) }}">Edit</a>
+                                <form action="{{ route('admin.posts.destroy', $post->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input class="btn btn-danger" type="submit" value="Cancella">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                {{ $posts->links() }}
+            </div>
+        </div>
+    </div>
+@endsection
