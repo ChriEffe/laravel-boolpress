9c6911e3f62b8c711fda60523b1ec3f4d9f14809
ChriEffe <christianferrazzano93@gmail.com>
2022-03-10 17:35:15 +0100

    vue router, modify front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -14,8 +14,51 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 import App from './views/App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import Posts from './pages/Posts';
+import Post from './pages/Post';
+
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/posts',
+            name: 'posts',
+            component: Posts
+        },
+        {
+            path: '/posts/:id',
+            name: 'post',
+            props: true,
+            component: Post
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: Contacts
+        },
+
+    ]
+})
 
 const app = new Vue({
     el: '#app',
     render: h => h(App),
+    router
 });
